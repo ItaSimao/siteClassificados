cd73bc84ffdc61c260c86e669938dac17caadc8c
Itamar Simão <102179953+ItaSimao@users.noreply.github.com>
2025-05-03 11:13:56 -0300

    estudo

--- pages/cadastre-se.php
+++ pages/cadastre-se.php
@@ -1,5 +1,12 @@
 <?php require __DIR__ . '/header.php'; ?>
 <div class="container">
-    <!-- Conteúdo -->
+
+    <h1>Cadastre-se</h1>
+    <form method="POST" action="cadastre-se.php">
+        <div class="form-group">
+            <label for="nome">Nome:</label>
+            <input type="text" class="form-control" id="nome" name="nome" required>
+        </div>
+        <div class="form-group
 </div>
 <?php require __DIR__ . '/footer.php'; ?>
